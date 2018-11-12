b52777818f64c5bce016993b1102af4015cdfc4d
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-12 16:02:04 +0000

    P params

--- lib/app.rb
+++ lib/app.rb
@@ -10,7 +10,13 @@ get '/secret' do
   "I'm not stupid, u stupid!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
