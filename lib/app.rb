77cc95f7a45777a1abc207bc35eec7b9cdaca643
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-12 15:12:56 +0000

    Added "cat"

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "Hello bitch, it's Pickle Rick here!"
 end
 
 get '/secret' do
   "I'm not stupid, u stupid!"
+end
+
+get '/cat' do
+  "<div style='border: dashed red;'>
+  <img src='https://i.imgur.com/M18NkL5.jpg'>
+  </div>"
 end
